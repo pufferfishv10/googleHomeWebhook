43cb0c1759cb09cb6da7982ef29c949fed02356a
Vinod, Ganesh <ganesh.vinod@accenture.com>
2017-01-12 11:40:51 +0000

    made changes to payload

--- app/controllers/SSNController.java
+++ app/controllers/SSNController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import model.Fulfillment;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -37,15 +38,20 @@ public class SSNController extends Controller {
 
     public Result save() throws IOException {
         JsonNode data = request().body().asJson();
-        JsonNode params = data.get("parameters");
+
+
+        JsonNode result = data.get("result");
+        JsonNode params = result.get("parameters");
+
+
+
         String cookingSpeed =  params.get("cooking-speed").asText();
         String  protein = params.get("protein").asText();
         String res = RestClient.getRecipe(protein);
 
 
 
-
-
+/*
         ///////////////
         ObjectMapper mapper = new ObjectMapper();
         JsonNode actualObj = mapper.readTree(res);
@@ -57,18 +63,26 @@ public class SSNController extends Controller {
         JsonNode recipe = zero.get("recipe");
 
         String chef = recipe.get("source").asText();
-        String label = recipe.get("label").asText();
+        String label = recipe.get("label").asText();*/
 
+        JsonNode fulfillment = result.get("fulfillment");
 
-        Fulfillment f = new Fulfillment();
-        f.setDisplaytext(label);
+        ((ObjectNode) fulfillment).put("speech", "Today in Boston: Fair, the temperature is 37 F");
+
+        /*"speech": "Today in Boston: Fair, the temperature is 37 F",
+                "source": "apiai-weather-webhook-sample",
+                "displayText": "Today in Boston: Fair, the temperature is 37 F"
+*/
+
+        /*Fulfillment f = new Fulfillment();
+        f.setDisplaytext(label + "cooking speed is"+ cookingSpeed );
         f.setSource(chef);
         f.setSpeech("I found the " + label);
 
-        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(f);
+        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(f);*/
         /////////////////
 
-            return ok(json);
+            return ok(data);
 
     }
 
